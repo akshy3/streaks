4d753d2fde9b67353fe8cb5a5578b67d02fbca5e
akshy3 <akshayashok209@gmail.com>
2023-06-12 17:50:34 +0530

    Added add activity button

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import ActivityScreen from './screens/ActivityScreen';
+import AddActivityScreen from './screens/AddActivityScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title: "Streaks"}}/>
+      <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Streaks" }} />
+          <Stack.Screen name="Details" component={ActivityScreen} options={{ title: "Details" }} />
+          <Stack.Screen name="AddActivity" component={AddActivityScreen} screenOptions={{presentation: 'modal'}} />
+
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState } from "react";
 import Activity from "../components/Activity";
 import uuid from 'react-native-uuid';
 
-var anActivity = {"activities":[{"id": uuid.v4(),"name": "random","start": Date.now()},{"id": uuid.v4(),"name": "hello","start": Date.now()}]}
+var anActivity = { "activities": [{ "id": uuid.v4(), "name": "random", "start": Date.now() }, { "id": uuid.v4(), "name": "hello", "start": Date.now() }] }
 
 const storeData = async (value: object) => {
   try {
@@ -12,42 +12,44 @@ const storeData = async (value: object) => {
     await AsyncStorage.setItem('@activities', jsonValue)
   } catch (e) {
     // saving error
-    }
   }
+}
 
-  
-  const getData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem('@activities')
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch(e) {
-      // error reading value
-    }
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@activities')
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
   }
+}
 
-  const dothat = async ()=>{
-    await storeData(anActivity)
-    let a= await getData();
-    console.log(a)
-  }
-  export default function HomeScreen() {
+const dothat = async () => {
+  await storeData(anActivity)
+  let a = await getData();
+  console.log(a)
+}
+export default function HomeScreen({navigation}) {
 
-    const [activities, setActivities] = useState(anActivity)
-    dothat();
+  const [activities, setActivities] = useState(anActivity)
+  dothat();
 
-    
-    let val = getData();
-    console.log(activities)
-    return (<View style={styles.container}>
-        <View>{activities.activities.map((e,index)=>(<Activity id={e.id} activities={activities} key={index}/>))}</View>
-    </View>
-    )
+
+  let val = getData();
+  console.log(activities)
+  return (<View style={styles.container}>
+    <View>{activities.activities.map((e, index) => (<Activity id={e.id} activities={activities} key={index} />))}</View>
+    <Button title="Add activity" onPress={()=>navigation.navigate('AddActivity')}/>
+
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'black',
-        flex: 1,
-        flexDirection: 'column',
-    }
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    flexDirection: 'column',
+  }
 })
