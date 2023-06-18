7544be97789741805a7484c4f4934f5e62dcad3a
akshy3 <akshayashok209@gmail.com>
2023-06-18 14:22:19 +0530

    Added timeline flatlist
    for viewing history.

--- screens/ActivityScreen.tsx
+++ screens/ActivityScreen.tsx
@@ -1,33 +1,37 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Button, StyleSheet, Text, View } from "react-native";
+import Timeline from 'react-native-timeline-flatlist'
+
 
 export default function ActivityScreen(props: { route: any; navigation: any; }) {
     const { route, navigation, } = props
     const { id, activities } = route.params;
     var activity = activities.find((item: { id: any; }) => item.id == id);
     let date = new Date(activity.date)
+    const timelinedata: { time: string; title: string; description: string; }[] = []
+
 
     const DateDiff = (date1: number, date2: number): number => {
         let diff = Math.floor((date1 - date2) / 86400000);
         return diff;
     }
     const handleHistory = (arr: number[]) => {
-        let res = [];
         for (let i = 0; i < arr.length; i++) {
             if (i == 0) {
                 let diff = DateDiff(Date.now(), arr[i]);
-                res.push(<Text>{diff} Days</Text>)
+                timelinedata.push({time: ``, title: `${diff} Days`, description:(new Date(arr[i]).toDateString())})
 
             }
             else {
 
                 let diff = DateDiff(arr[i - 1], arr[i]);
-                res.push(<Text>{diff} Days</Text>)
+                timelinedata.push({time: '', title: `${diff} Days`, description:(new Date(arr[i]).toDateString())})
 
             }
         }
-        return <View style={styles.historyContainer}>{res}</View>
+        
     }
+    handleHistory(activity.history)
 
     const handeDelete = async () => {
         let data = activities.filter((item: { id: any; }) => item.id !== id)
@@ -48,7 +52,10 @@ export default function ActivityScreen(props: { route: any; navigation: any; }) 
         <View style={styles.container}>
             <Text style={styles.text}>{activity.title}</Text>
             <Text style={styles.text}>{date.toDateString()}</Text>
-            <Text style={styles.text}>{activity.history ? handleHistory(activity.history) : <></>}</Text>
+            {/* <Text style={styles.text}>{activity.history ? handleHistory(activity.history) : <></>}</Text> */}
+            <Timeline data={timelinedata} options={{
+                style: { backgroundColor: 'white' }
+            }} />
             <Button onPress={handleRelapse} title="Relapse" />
             <Button onPress={handeDelete} title="Delete this activity" />
         </View>
@@ -57,14 +64,13 @@ export default function ActivityScreen(props: { route: any; navigation: any; }) 
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'black',
+        backgroundColor: 'white',
         flex: 1,
         padding: 10,
     },
     text: {
-        color: 'white',
     },
-    historyContainer:{
+    historyContainer: {
         backgroundColor: 'yellow',
     }
 })
