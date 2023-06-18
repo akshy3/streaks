777f5e825fd6e106960d81f87dd7e2a776f6a73c
akshy3 <akshayashok209@gmail.com>
2023-06-18 13:21:58 +0530

    History formatted

--- screens/ActivityScreen.tsx
+++ screens/ActivityScreen.tsx
@@ -6,6 +6,29 @@ export default function ActivityScreen(props: { route: any; navigation: any; }) 
     const { id, activities } = route.params;
     var activity = activities.find((item: { id: any; }) => item.id == id);
     let date = new Date(activity.date)
+
+    const DateDiff = (date1: number, date2: number): number => {
+        let diff = Math.floor((date1 - date2) / 86400000);
+        return diff;
+    }
+    const handleHistory = (arr: number[]) => {
+        let res = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                let diff = DateDiff(Date.now(), arr[i]);
+                res.push(<Text>{diff} Days</Text>)
+
+            }
+            else {
+
+                let diff = DateDiff(arr[i - 1], arr[i]);
+                res.push(<Text>{diff} Days</Text>)
+
+            }
+        }
+        return <View style={styles.historyContainer}>{res}</View>
+    }
+
     const handeDelete = async () => {
         let data = activities.filter((item: { id: any; }) => item.id !== id)
         data = JSON.stringify(data)
@@ -14,8 +37,8 @@ export default function ActivityScreen(props: { route: any; navigation: any; }) 
     }
     const handleRelapse = async () => {
         let data = activities.filter((item: { id: any; }) => item.id !== id)
-        let newHistory = [...activity.history,Date.now()]
-        data = [...data, { id: id, title: activity.title, date: Date.now(), history: newHistory}]
+        let newHistory = [...activity.history, Date.now()]
+        data = [...data, { id: id, title: activity.title, date: Date.now(), history: newHistory }]
 
         data = JSON.stringify(data)
         await AsyncStorage.setItem('@activities', data)
@@ -25,7 +48,7 @@ export default function ActivityScreen(props: { route: any; navigation: any; }) 
         <View style={styles.container}>
             <Text style={styles.text}>{activity.title}</Text>
             <Text style={styles.text}>{date.toDateString()}</Text>
-            <Text style={styles.text}>{activity.history}</Text>
+            <Text style={styles.text}>{activity.history ? handleHistory(activity.history) : <></>}</Text>
             <Button onPress={handleRelapse} title="Relapse" />
             <Button onPress={handeDelete} title="Delete this activity" />
         </View>
@@ -36,8 +59,12 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: 'black',
         flex: 1,
+        padding: 10,
     },
     text: {
         color: 'white',
+    },
+    historyContainer:{
+        backgroundColor: 'yellow',
     }
 })
